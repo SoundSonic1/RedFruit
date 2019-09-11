9a1ffce3488be6d12e5bccd15ebf77dee55d4e23
Kien Dang Tran <kiendangtran0@gmail.com>
2019-09-11 18:06:46 +0200

    data: RedditImage: change resolutions to List

--- app/src/main/java/com/example/redfruit/data/model/images/RedditImage.kt
+++ app/src/main/java/com/example/redfruit/data/model/images/RedditImage.kt
@@ -4,4 +4,4 @@ package com.example.redfruit.data.model.images
  * @property source original image
  * @property resolutions various (compressed) versions of the image
  */
-data class RedditImage(val source: ImageSource, val resolutions: Collection<ImageSource>)
+data class RedditImage(val source: ImageSource, val resolutions: List<ImageSource>)
