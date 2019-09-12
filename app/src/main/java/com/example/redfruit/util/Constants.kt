518790edd74abda2ebfa998aba0a1dd86bbd924c
Kien Dang Tran <kiendangtran0@gmail.com>
2019-09-12 15:59:48 +0200

    util: Constants: update user agent according to api docs

--- app/src/main/java/com/example/redfruit/util/Constants.kt
+++ app/src/main/java/com/example/redfruit/util/Constants.kt
@@ -6,6 +6,6 @@ package com.example.redfruit.util
 object Constants {
     const val DEFAULT_SUB = "androiddev"
     const val BASE_URL = "https://www.reddit.com/r/"
-    const val USER_AGENT =
-        "Mozilla/5.0 (Linux; Android 9; Pixel Build/PQ3A.190705.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.101 Mobile Safari/537.36"
+    // https://github.com/reddit-archive/reddit/wiki/api
+    const val USER_AGENT = "android:com.example.redfruit:v1.0 (by /u/SoundSonic1)"
 }
