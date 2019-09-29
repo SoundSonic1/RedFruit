2b574f0070fa8bf0f24ae33349fdc278a3a476b9
Kien Dang Tran <kiendangtran0@gmail.com>
2019-09-29 12:50:31 +0200

    ui: base: FullScreenFragment: hide nav bar

--- app/src/main/java/com/example/redfruit/ui/base/FullScreenFragment.kt
+++ app/src/main/java/com/example/redfruit/ui/base/FullScreenFragment.kt
@@ -1,5 +1,6 @@
 package com.example.redfruit.ui.base
 
+import android.view.View
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
@@ -32,10 +33,37 @@ abstract class FullScreenFragment : Fragment() {
     override fun onResume() {
         super.onResume()
         disableToolbar(activity!!)
+        hideSystemUI()
     }
 
     override fun onPause() {
         super.onPause()
         enableToolbar(activity!!)
+        showSystemUI()
+    }
+
+    private fun showSystemUI() {
+        activity?.window?.decorView.apply {
+            this?.let {
+                systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+            }
+        }
+    }
+
+    private fun hideSystemUI() {
+        activity?.window?.decorView.apply {
+            // Hide both the navigation bar and the status bar.
+            // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+            // a general rule, you should design your app to hide the status bar whenever you
+            // hide the navigation bar.
+            this?.let {
+                systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_FULLSCREEN)
+            }
+        }
     }
 }
