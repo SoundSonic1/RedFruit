98c11f3566705e641bdffee2594bd1672eb8a087
Kien Dang Tran <Kien.Dang_Tran@mailbox.tu-dresden.de>
2019-11-18 19:49:38 +0100

    ui: remove settings in toolbar

--- app/src/main/java/com/example/redfruit/ui/MainActivity.kt
+++ app/src/main/java/com/example/redfruit/ui/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.redfruit.ui
 
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatDelegate
@@ -116,23 +115,6 @@ class MainActivity : DaggerAppCompatActivity(), NavigationView.OnNavigationItemS
             drawer_layout.closeDrawer(GravityCompat.START)
         } else {
             super.onBackPressed()
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
         }
     }
 
